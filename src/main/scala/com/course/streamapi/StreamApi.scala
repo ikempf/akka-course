0b19991cf8c44da00379beda8b4f011daeb50e82
Ilja <ilja.kempf@gmail.com>
2019-04-21 10:49:14 +0200

    Improve streamapi example

--- src/main/scala/com/course/streamapi/StreamApi.scala
+++ src/main/scala/com/course/streamapi/StreamApi.scala
@@ -3,7 +3,10 @@ package com.course.streamapi
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
+import com.course.streamapi.Stub.saveToDb
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object StreamApi extends App {
@@ -11,12 +14,29 @@ object StreamApi extends App {
   implicit val as: ActorSystem       = ActorSystem()
   implicit val am: ActorMaterializer = ActorMaterializer()
 
-  val single = Source.single(0)
-  val list   = Source.apply(List(1, 2, 3))
-  val const  = Source.repeat(0)
-  val tick   = Source.tick(500.milliseconds, 1.second, -1)
-  val fib    = Source.unfold((0, 1)) { case (a, b) => Some(((b, a + b), a)) }
+  // Construct sources
+  Source.single(0)
+  Source.apply(List(1, 2, 3))
+  Source.repeat(0)
+  Source.tick(500.milliseconds, 1.second, -1)
+  Source.unfold((0, 1)) { case (a, b) => Some(((b, a + b), a)) }
 
-  fib.take(10).runWith(Sink.foreach(println))
+  // Intermediate manipulation
+  val source = Source.empty[Int]
+  source.map(_ + 1)
+  source.mapAsync(parallelism = 50)(saveToDb)
+  source.flatMapConcat(i => Source.apply(List(i, i, i)))
+  source.take(15)
+  source.filter(_ % 2 == 0)
+  source.grouped(5).map(group => group.sum)
 
+  // Final operations
+  source.runFold(0)(_ + _)
+  source.runForeach(println)
+  source.runWith(Sink.ignore)
+
+}
+
+object Stub {
+  def saveToDb(i: Int): Future[Int] = Future(i)
 }
